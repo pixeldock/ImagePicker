7ce026c93b91423f36c3bd4511c22b838eeeed2b
Jörn Schoppe <joern@pixeldock.com>
2018-04-07 10:46:53 +0200

    Fix Swift 4.1 errors

--- Source/ImageGallery/ImageGalleryLayout.swift
+++ Source/ImageGallery/ImageGalleryLayout.swift
@@ -21,9 +21,9 @@ class ImageGalleryLayout: UICollectionViewFlowLayout {
     var newAttributes = [UICollectionViewLayoutAttributes]()
     for attribute in attributes {
       // swiftlint:disable force_cast
-      let n = attribute.copy() as! UICollectionViewLayoutAttributes
-      n.transform = configuration.rotationTransform
-      newAttributes.append(n)
+      let newAttribute = attribute.copy() as! UICollectionViewLayoutAttributes
+      newAttribute.transform = configuration.rotationTransform
+      newAttributes.append(newAttribute)
     }
 
     return newAttributes
